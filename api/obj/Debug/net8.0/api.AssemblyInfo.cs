8a3e05ca909d875c93500ec885b4625517492d8d
Van Dung Dev <vincentcode402112@gmail.com>
2024-10-30 13:56:47 +0700

    push

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee7272212dd6ece36ceffe2a56cd60b9982880ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14ca5c1a3e46e1de17d3844271dbbab380e9121e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
