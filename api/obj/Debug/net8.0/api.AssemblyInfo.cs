6cef90ef20b6b5c18cd96e1766100dc87a3d6f6f
Van Dung Dev <vincentcode402112@gmail.com>
2024-10-30 14:01:06 +0700

    push code from DungKen branch

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+739c8ad6555db966e182a62f5db0133269bfb008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37eb8496d5f4e29b4da26ccc4b26c879feef28a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
